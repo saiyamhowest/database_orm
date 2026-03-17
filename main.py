bfaeaf7d772f487677edb3f404a74678cdad28b2
saiyamhowest <saiyam.sharma@student.howest.be>
2026-03-17 08:06:06 +0100

    feat: implement Birdspotting API with linked bird data

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI
 from database import start_db
 from routers import species
 from routers import birds
+from routers import birdspotting
 
 app = FastAPI()
 
 app.include_router(birds.router)
 
 app.include_router(species.router)
+
+app.include_router(birdspotting.router)
 
 @app.on_event("startup")
 def on_startup():
